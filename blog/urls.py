0967b9bb6f31f6acd1c062f9b7b6c5d7581f59c7
masoudkazak <kzmasut@gmail.com>
2021-07-21 18:47:37 +0430

    create accounts app

--- blog/urls.py
+++ blog/urls.py
@@ -14,11 +14,11 @@ from .views import (
 app_name = 'blog'
 
 urlpatterns = [
-    path('home/', PostListView.as_view(), name='homepage'),
-    path("home/<str:author>/<str:title>/<int:pk>/", PostDetailView.as_view(), name="detail-post"),
-    path('home/<str:author>/<str:title>/<int:pk>/delete/', PostDeleteView.as_view(), name='delete-view'),
-    path('home/<str:author>/<str:title>/<int:pk>/update/', PostUpdateView.as_view(), name='update-view'),
-    path('home/create-post/', PostCreateView.as_view()),
-    path("home/<int:pk>/like/", LikeView, name="like_post"),
-    path('home/<int:pk>/comment/', CommentCreateView.as_view(), name='addcomment')
+    path('', PostListView.as_view(), name='homepage'),
+    path("<str:author>/<str:title>/<int:pk>/", PostDetailView.as_view(), name="detail-post"),
+    path('<str:author>/<str:title>/<int:pk>/delete/', PostDeleteView.as_view(), name='delete-view'),
+    path('<str:author>/<str:title>/<int:pk>/update/', PostUpdateView.as_view(), name='update-view'),
+    path('create-post/', PostCreateView.as_view()),
+    path("<int:pk>/like/", LikeView, name="like_post"),
+    path('<int:pk>/comment/', CommentCreateView.as_view(), name='addcomment')
 ]
